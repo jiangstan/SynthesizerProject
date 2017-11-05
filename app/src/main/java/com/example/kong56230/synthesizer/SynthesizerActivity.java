d7fcf27b70762bf2ece53ab494198a53d61440a8
Stanley Jiang <30996821+jiangstan@users.noreply.github.com>
2017-11-05 13:59:55 -0800

    Add files via upload

--- app/src/main/java/com/example/kong56230/synthesizer/SynthesizerActivity.java
+++ app/src/main/java/com/example/kong56230/synthesizer/SynthesizerActivity.java
@@ -6,332 +6,101 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
 import java.util.ArrayList;
 
 public class SynthesizerActivity extends AppCompatActivity {
-    private static final String TAG = SynthesizerActivity.class.getName();
-   
-    public Button button1;
-    public Button button2;
-    public MediaPlayer mpA;
-    public MediaPlayer mpB;
-    public MediaPlayer mpC;
-    public MediaPlayer mpCs;
-    public MediaPlayer mpD;
-    public MediaPlayer mpE;
-    public MediaPlayer mpHighE;
-    public MediaPlayer mpF;
-    public MediaPlayer mpHFs;
-    private MediaPlayer mpFs;
-    private MediaPlayer mpG;
-    private final int WHOLE_NOTE = 1000;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_synthesizer);
-
-        Button button1 = (Button) findViewById(R.id.Button1);
-        Button button2 = (Button) findViewById(R.id.Button2);
-
-        mpA = MediaPlayer.create(this, R.raw.scalea);
-        mpB = MediaPlayer.create(this, R.raw.scaleb);
-        mpC = MediaPlayer.create(this, R.raw.scalec);
-        mpCs = MediaPlayer.create(this, R.raw.scalecs);
-        mpD = MediaPlayer.create(this, R.raw.scaled);
-        mpE = MediaPlayer.create(this, R.raw.scalee);
-        mpHighE = MediaPlayer.create(this, R.raw.scalehighe);
-        mpF = MediaPlayer.create(this, R.raw.scalef);
-        mpFs = MediaPlayer.create(this, R.raw.scalefs);
-        mpHFs = MediaPlayer.create(this, R.raw.scalehighfs);
-        mpG = MediaPlayer.create(this, R.raw.scaleg);
-    }
+        private static final String TAG = SynthesizerActivity.class.getName();
+        public Button button1;
+        public Button button2;
+        public MediaPlayer mpA;
+        public MediaPlayer mpB;
+        public MediaPlayer mpC;
+        public MediaPlayer mpCs;
+        public MediaPlayer mpD;
+        public MediaPlayer mpE;
+        public MediaPlayer mpEE;
+        public MediaPlayer mpF;
+        public MediaPlayer mpFF;
+        private final int WHOLE_NOTE = 1000;
+        ArrayList<MediaPlayer> twinklestar;
 
 
 
-    private void delayPlaying(int delay) throws InterruptedException {
-        Log.e("SynthesizerActivity", "Challenge 0 Button clicked");
-        try {
-            Thread.sleep(delay); //
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_synthesizer);
+
+            Button button1 = (Button) findViewById(R.id.adelebutton);
+            mpA = MediaPlayer.create(this, R.raw.scalea);
+            mpEE = MediaPlayer.create(this, R.raw.scalef);
+            mpFF = MediaPlayer.create(this, R.raw.scalehighfs);
+            mpB = MediaPlayer.create(this, R.raw.scaleb);
+            mpC = MediaPlayer.create(this, R.raw.scalec);
+            mpCs = MediaPlayer.create(this, R.raw.scalecs);
+            mpD = MediaPlayer.create(this, R.raw.scaled);
+            mpE = MediaPlayer.create(this, R.raw.scalee);
+            mpF = MediaPlayer.create(this, R.raw.scalef);
+
+            twinklestar = new ArrayList<>();
+
+            twinklestar.add(mpA);
+            twinklestar.add(mpA);
+            twinklestar.add(mpEE);
+            twinklestar.add(mpEE);
+            twinklestar.add(mpFF);
+            twinklestar.add(mpFF);
+            twinklestar.add(mpEE);
+            twinklestar.add(mpD);
+            twinklestar.add(mpD);
+            twinklestar.add(mpCs);
+            twinklestar.add(mpCs);
+            twinklestar.add(mpB);
+            twinklestar.add(mpB);
+            twinklestar.add(mpA);
+        }
+
+        private void delayPlaying(int delay) throws InterruptedException{
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                Log.e("SynthesizerActivity","Audio playback interrupted");
+            }
         }
     }
-    public void onButton1Click(View v) throws InterruptedException {
+
+    private void delayPlaying(int delay) throws InterruptedException{
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            Log.e("SynthesizerActivity","Audio playback interrupted");
+        }
+    }
+
+    public void onButton1Click(View v) {
         mpE.seekTo(0);
         Log.i(TAG,"Button 1 Clicked");
-        delayPlaying(WHOLE_NOTE/2);
         mpE.start();
-        delayPlaying(WHOLE_NOTE/2);
-        mpFs.start();
-        delayPlaying(WHOLE_NOTE/2);
-        mpG.start();
-        delayPlaying(WHOLE_NOTE/2);
-        mpA.start();
-        delayPlaying(WHOLE_NOTE/2);
-        mpB.start();
-        delayPlaying(WHOLE_NOTE/2);
-        mpCs.start();
-        delayPlaying(WHOLE_NOTE/2);
-        mpD.start();
-        delayPlaying(WHOLE_NOTE/2);
-        mpE.start();
-        delayPlaying(WHOLE_NOTE/2);
 
     }
-
-    public void onButton2Click(View v) throws InterruptedException {
-
-        mpA.seekTo(0);
+    public void onButton2Click(View v) {
+        mpF.seekTo(0);
         Log.i(TAG,"Button 2 Clicked");
-        delayPlaying(WHOLE_NOTE/2);
-        mpA.start();
-        delayPlaying(WHOLE_NOTE);
-        mpA.pause();
+        mpF.start();
 
-        mpA.seekTo(0);
-        mpA.start();
-        delayPlaying(WHOLE_NOTE);
-        mpA.pause();
-
-        mpHighE.seekTo(0);
-        mpHighE.start();
-        delayPlaying(WHOLE_NOTE);
-        mpHighE.pause();
-
-        mpHighE.seekTo(0);
-        mpHighE.start();
-        delayPlaying(WHOLE_NOTE);
-        mpHighE.pause();
-
-        mpHFs.seekTo(0);
-        mpHFs.start();
-        delayPlaying(WHOLE_NOTE);
-        mpHFs.pause();
-
-        mpHFs.seekTo(0);
-        mpHFs.start();
-        delayPlaying(WHOLE_NOTE);
-        mpHFs.pause();
-
-        mpHighE.seekTo(0);
-        mpHighE.start();
-        delayPlaying(WHOLE_NOTE);
-        mpHighE.pause();
-
-        mpD.seekTo(0);
-        mpD.start();
-        delayPlaying(WHOLE_NOTE);
-        mpD.pause();
-
-        mpD.seekTo(0);
-        mpD.start();
-        delayPlaying(WHOLE_NOTE);
-        mpD.pause();
-
-        mpCs.seekTo(0);
-        mpCs.start();
-        delayPlaying(WHOLE_NOTE);
-        mpCs.pause();
-
-        mpCs.seekTo(0);
-        mpCs.start();
-        delayPlaying(WHOLE_NOTE);
-        mpCs.pause();
-
-        mpB.seekTo(0);
-        mpB.start();
-        delayPlaying(WHOLE_NOTE);
-        mpB.pause();
-
-        mpB.seekTo(0);
-        mpB.start();
-        delayPlaying(WHOLE_NOTE);
-        mpB.pause();
-
-        mpA.seekTo(0);
-        mpA.start();
-        delayPlaying(WHOLE_NOTE);
-        mpA.pause();
-        }
-        public void onButton3Click(View V) throws InterruptedException{
-            mpA.seekTo(0);
-            Log.i(TAG,"Button 3 Clicked");
-            mpA.start();
-            delayPlaying(WHOLE_NOTE);
-            mpA.pause();
-            mpA.seekTo(0);
-            mpA.start();
-            delayPlaying(WHOLE_NOTE);
-            mpA.pause();
-            mpA.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpHFs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHFs.pause();
-            mpHFs.seekTo(0);
-            mpHFs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHFs.pause();
-            mpHFs.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpD.start();
-            delayPlaying(WHOLE_NOTE);
-            mpD.pause();
-            mpD.seekTo(0);
-            mpD.start();
-            delayPlaying(WHOLE_NOTE);
-            mpD.pause();
-            mpD.seekTo(0);
-            mpCs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpCs.pause();
-            mpCs.seekTo(0);
-            mpCs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpCs.pause();
-            mpCs.seekTo(0);
-            mpB.start();
-            delayPlaying(WHOLE_NOTE);
-            mpB.pause();
-            mpB.seekTo(0);
-            mpB.start();
-            delayPlaying(WHOLE_NOTE);
-            mpB.pause();
-            mpB.seekTo(0);
-            mpA.start();
-            delayPlaying(WHOLE_NOTE);
-            delayPlaying(WHOLE_NOTE);
-            mpA.pause();
-            mpA.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpD.start();
-            delayPlaying(WHOLE_NOTE);
-            mpD.pause();
-            mpD.seekTo(0);
-            mpD.start();
-            delayPlaying(WHOLE_NOTE);
-            mpD.pause();
-            mpD.seekTo(0);
-            mpCs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpCs.pause();
-            mpCs.seekTo(0);
-            mpCs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpCs.pause();
-            mpCs.seekTo(0);
-            mpB.start();
-            delayPlaying(WHOLE_NOTE);
-            delayPlaying(WHOLE_NOTE);
-            mpB.pause();
-            mpB.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpD.start();
-            delayPlaying(WHOLE_NOTE);
-            mpD.pause();
-            mpD.seekTo(0);
-            mpD.start();
-            delayPlaying(WHOLE_NOTE);
-            mpD.pause();
-            mpD.seekTo(0);
-            mpCs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpCs.pause();
-            mpCs.seekTo(0);
-            mpCs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpCs.pause();
-            mpCs.seekTo(0);
-            mpB.start();
-            delayPlaying(WHOLE_NOTE);
-            delayPlaying(WHOLE_NOTE);
-            mpB.pause();
-            mpB.seekTo(0);
-            mpA.start();
-            delayPlaying(WHOLE_NOTE);
-            mpA.pause();
-            mpA.seekTo(0);
-            mpA.start();
-            delayPlaying(WHOLE_NOTE);
-            mpA.pause();
-            mpA.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpHFs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHFs.pause();
-            mpHFs.seekTo(0);
-            mpHFs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpHFs.pause();
-            mpHFs.seekTo(0);
-            mpHighE.start();
-            delayPlaying(WHOLE_NOTE);
-            delayPlaying(WHOLE_NOTE);
-            mpHighE.pause();
-            mpHighE.seekTo(0);
-            mpD.start();
-            delayPlaying(WHOLE_NOTE);
-            mpD.pause();
-            mpD.seekTo(0);
-            mpD.start();
-            delayPlaying(WHOLE_NOTE);
-            mpD.pause();
-            mpD.seekTo(0);
-            mpCs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpCs.pause();
-            mpCs.seekTo(0);
-            mpCs.start();
-            delayPlaying(WHOLE_NOTE);
-            mpCs.pause();
-            mpCs.seekTo(0);
-            mpB.start();
-            delayPlaying(WHOLE_NOTE);
-            mpB.pause();
-            mpB.seekTo(0);
-            mpB.start();
-            delayPlaying(WHOLE_NOTE);
-            mpB.pause();
-            mpB.seekTo(0);
-            mpA.start();
-            delayPlaying(WHOLE_NOTE);
-            delayPlaying(WHOLE_NOTE);
-            mpA.pause();
-            mpA.seekTo(0);
-
-        }
     }
-
+    mChallenge1.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        Log.e("SynthesizerActivity", "Challenge 0 Button clicked");
+        try {
+        mpE.start();
+        delayPlaying(WHOLE_NOTE);
+        mpF.start();
+        } catch (InterruptedException e) {
+        e.printStackTrace();
+        }
+        }
+        });
+}
